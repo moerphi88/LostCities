343e3745795bc0f20ac3d81bf24f574989a0dff4
moerphi88 <moerphi@gmx.de>
2018-03-18 22:04:05 +0100

    Aktualisierung der View nach Abheben einer Karte. Logik für Abheben implementiert.

--- LostCities/LostCities/ViewModel/AblagestapelViewModel.cs
+++ LostCities/LostCities/ViewModel/AblagestapelViewModel.cs
@@ -19,6 +19,9 @@ namespace LostCities.ViewModel
         private string _gruenerStapelImageUri;
         private string _blauerStapelImageUri;
         private string _weißerStapelImageUri;
+        //TODO Hier könnte ich ein dict einführen. Key wäre die Farbe. und value eine Liste von Cards. Nur die oberste würde angezeigt werden.
+        private List<Card> _cardList;
+        private Dictionary<Farbe, List<Card>> _ablagestapel;
 
         public delegate void CardEventHandler(object sender, CardEventArgs e);
         public event CardEventHandler KarteAbheben;
@@ -31,12 +34,21 @@ namespace LostCities.ViewModel
         public AblagestapelViewModel(INavigation navigation) : base(navigation)
         {
             OnButtonPressedCommand = new Command<string>(OnButtonPressed);
+
+            _ablagestapel = new Dictionary<Farbe, List<Card>>();
+            _ablagestapel.Add(Farbe.Herz, new List<Card>());
+            _ablagestapel.Add(Farbe.Karo, new List<Card>());
+            _ablagestapel.Add(Farbe.Kreuz, new List<Card>());
+            _ablagestapel.Add(Farbe.Pik, new List<Card>());
+
+
             GelberStapelImageUri = "kartenhindergrund.png";
             BlauerStapelImageUri = "kartenhindergrund.png";
             GruenerStapelImageUri = "kartenhindergrund.png";
             RoterStapelImageUri = "kartenhindergrund.png";
             WeißerStapelImageUri = "kartenhindergrund.png";
         }
+
         public ICommand OnButtonPressedCommand { get; }
 
         async void OnButtonPressed(string value)
@@ -47,8 +59,28 @@ namespace LostCities.ViewModel
             {
                 if (answer)
                 {
-                    var CardEventArgs = new CardEventArgs(new Card());
-                        OnKarteAbheben(CardEventArgs);
+                    var farbe = Farbe.Herz;
+                    switch (value)
+                    {
+                        case "0":
+                            farbe = Farbe.Herz;
+                            break;
+                        case "1":
+                            farbe = Farbe.Karo;
+                            break;
+                        case "2":
+                            farbe = Farbe.Pik;
+                            break;
+                        case "3":
+                            farbe = Farbe.Kreuz;
+                            break;
+                    }
+                    if (_ablagestapel[farbe].Count != 0)
+                    {
+                        OnKarteAbheben(CreateCardEventArgs(farbe));
+                        _ablagestapel[farbe].RemoveAt(_ablagestapel[farbe].Count-1);
+                        UpdateImageUri(farbe);
+                    }
                 }
             }
             catch (Exception e)
@@ -57,21 +89,25 @@ namespace LostCities.ViewModel
             }
         }
 
-        public void KarteAblegen(Card Card)
+        public void KarteAblegen(Card card)
         {
-            switch (Card.Name)
+            switch (card.Name)
             {
                 case "Herz":
-                    GelberStapelImageUri = Card.ImageUri;
+                    _ablagestapel[Farbe.Herz].Add(card);
+                    GelberStapelImageUri = SetImageUri(Farbe.Herz);
                     break;
                 case "Karo":
-                    BlauerStapelImageUri = Card.ImageUri;
+                    _ablagestapel[Farbe.Karo].Add(card);
+                    BlauerStapelImageUri = SetImageUri(Farbe.Karo);
                     break;
                 case "Pik":
-                    GruenerStapelImageUri = Card.ImageUri;
+                    _ablagestapel[Farbe.Pik].Add(card);
+                    GruenerStapelImageUri = SetImageUri(Farbe.Pik);
                     break;
                 case "Kreuz":
-                    RoterStapelImageUri = Card.ImageUri;
+                    _ablagestapel[Farbe.Kreuz].Add(card);
+                    RoterStapelImageUri = SetImageUri(Farbe.Kreuz);
                     break;
                 default:
                     break;
@@ -148,6 +184,41 @@ namespace LostCities.ViewModel
             }
         }
 
+        private String SetImageUri(Farbe farbe)
+        {
+            if (_ablagestapel[farbe].Count != 0)
+            {
+                return _ablagestapel[farbe].ElementAt(_ablagestapel[farbe].Count - 1).ImageUri;
+            }
+            else return "kartenhindergrund.png";
+        }
+
+        private CardEventArgs CreateCardEventArgs(Farbe farbe)
+        {
+            var card = _ablagestapel[farbe].ElementAt(_ablagestapel[farbe].Count - 1);
+            return new CardEventArgs(card);
+        }
+
+        private void UpdateImageUri(Farbe farbe)
+        {
+            switch (farbe)
+            {
+                case Farbe.Herz:
+                    GelberStapelImageUri = SetImageUri(Farbe.Herz);
+                    break;
+                case Farbe.Karo:
+                    BlauerStapelImageUri = SetImageUri(Farbe.Karo);
+                    break;
+                case Farbe.Pik:
+                    GruenerStapelImageUri = SetImageUri(Farbe.Pik);
+                    break;
+                case Farbe.Kreuz:
+                    RoterStapelImageUri = SetImageUri(Farbe.Kreuz);
+                    break;
+                default:
+                    break;
+            }
+        }
 
     }
 }
