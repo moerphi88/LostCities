9e57b5b3f9f728f24e38a4a5018f1cfef3df2c7d
moerphi88 <moerphi@gmx.de>
2018-11-11 11:12:15 +0100

    created unit test project and adapted the nutGet references

---
+++ LostCities.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LostCities.Model;
+using LostCities.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LostCities.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CountPoints()
+        {
+            var sut = new AnlegestapelViewModel(null);
+            sut.KarteAnlegen(new Card(Farbe.Blau.ToString(), (int)Wert.Zwei));
+        }
+    }
+}
