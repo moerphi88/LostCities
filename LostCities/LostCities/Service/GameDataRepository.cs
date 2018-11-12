6c878277c149c1ce60ba1e346f9cc407202242fd
moerphi88 <moerphi@gmx.de>
2018-11-12 12:38:39 +0100

    added Preferences Xamarin.Essentials functionality

---
+++ LostCities/LostCities/Service/GameDataRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace LostCities.Service
+{
+    class GameDataRepository
+    {
+        public GameDataRepository()
+        {
+        }
+
+        public void SetMyKey(string value)
+        {
+            Set("my_key","Hallo");
+        }
+
+        public string GetMyKey()
+        {
+            return Get("my_key");
+        }
+
+        private void Set(string key, string value)
+        {
+            Preferences.Set(key, value);
+            Debug.WriteLine("Set");
+        }
+
+        private string Get(string key)
+        {
+            var s = Preferences.Get(key,"default");
+            Debug.WriteLine($"Get value:{s} for {key} ");
+            return s;         
+        }
+
+        private void Remove(string key)
+        {
+            Preferences.Remove(key);
+            Debug.WriteLine($"Delete {key}");
+        }
+
+        private void ClearAll()
+        {
+            Preferences.Clear();
+            Debug.WriteLine("ClearAll");
+        }
+    }
+}
--- LostCities/LostCities/ViewModel/MainViewModel.cs
+++ LostCities/LostCities/ViewModel/MainViewModel.cs
@@ -12,23 +12,38 @@ namespace LostCities.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+
+        //Warum sind die eigentlich public und als Properties?
         public DiscardPileViewModel DiscardPileVM { get; set; }
         public IStapel AnlegeStapelVM { get; set; }
         public IStapel AnlegeStapel2VM { get; set; }
         public HandViewModel HandVM { get; set; }
         public HandViewModel HandVM2 { get; set; }
         public LostCitiesGameLogic Lcgl { get; set; }
+        private GameDataRepository _gameDataRepository;
 
 
         public MainViewModel(INavigation navigation) : base(navigation)
         {
+            //TODO Hier beim erstellen muss ich checken, ob ein altes Spiel vorhanden ist. Kann ich ja auch mit einer Preference machen.
+            //
+
             DiscardPileVM = new DiscardPileViewModel(null);
-            //AnlegeStapelVM = new MauMauViewModel(null);
             AnlegeStapelVM = new AnlegestapelViewModel(null);
             AnlegeStapel2VM = new AnlegestapelViewModel(null);
             HandVM = new HandViewModel(null);
             HandVM2 = new HandViewModel(null);
             Lcgl = new LostCitiesGameLogic(HandVM, HandVM2, DiscardPileVM, AnlegeStapelVM, AnlegeStapel2VM);
+
+
+
+            _gameDataRepository = new GameDataRepository();
+
+            //Beim ersten Starten kommt hier noch "default" zurück
+            _gameDataRepository.GetMyKey();
+
+            _gameDataRepository.SetMyKey("HansWurst");
+
         }
     }
 }
